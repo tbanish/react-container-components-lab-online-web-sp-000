abc79110ad0c78d0e27fb2b65fb25d5cb4f2e7a4
tbanish <tombanish@gmail.com>
2021-03-30 21:32:21 -0400

    commit tree d374a0247fbeea5dbb291939de406ecfb0656b5a

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,33 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      reviews: []
+    }
+  }
+
+  render() {
+    return(
+      <div className="latest-movie-reviews">
+        <MovieReviews reviews={this.state.reviews}/>
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    fetch(URL)
+    .then(resp => resp.json())
+    .then(json => {
+      const reviews = json.results
+      this.setState({
+        reviews: reviews
+      })
+    })
+  }
+}
+
+export default LatestMovieReviewsContainer
